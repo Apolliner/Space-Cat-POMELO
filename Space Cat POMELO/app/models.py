d28fe4c94ee45646af4aa5fc39a6a0a3fd43df9b
Apolliner de Gray <plavec94@yandex.ru>
2021-02-24 21:11:40 +0300

    Добавление датабазы товаров, реализация её влияния на содержимое страниц.

--- Space Cat POMELO/app/models.py
+++ Space Cat POMELO/app/models.py
@@ -5,3 +5,51 @@ Definition of models.
 from django.db import models
 
 # Create your models here.
+
+
+
+
+product_db = [
+    {
+    'name': 'Pomelo microtonal guitar',
+    'description': ('Pomelo microtonal guitar Pomelo microtonal guitar Pomelo microtonal guitar Pomelo microtonal guitar Pomelo microtonal guitar '),
+    'image': 'http://www.pxleyes.com/images/contests/electric-guitar/infosteps/fullsize/56982_58c30f42a024d.jpg',
+    'price': '999',
+    },
+    {
+    'name': 'Pomelo stratocaster guitar',
+    'description': ('Pomelo stratocaster guitar Pomelo stratocaster guitar Pomelo stratocaster guitar Pomelo stratocaster guitar Pomelo stratocaster guitar '),
+    'image': 'https://www.soundaffects.com/images/products/zoom/1521113376-67605200.jpg',
+    'price': '999',
+    },
+    {
+    'name': 'Space cat microtonal guitar',
+    'description': ('Space cat microtonal guitar Space cat microtonal guitar Space cat microtonal guitar Space cat microtonal guitar Space cat microtonal guitar '),
+    'image': 'https://i.pinimg.com/originals/13/b3/82/13b3825859645172d6af71e6a9e69c37.jpg',
+    'price': '999',
+    },
+    {
+    'name': 'Space cat stratocaster guitar',
+    'description': ('Space cat stratocaster guitar Space cat stratocaster guitar Space cat stratocaster guitar Space cat stratocaster guitar Space cat stratocaster guitar '),
+    'image': 'https://images.reverb.com/image/upload/s--_DN8N4hm--/a_exif,c_limit,e_unsharp_mask:80,f_auto,fl_progressive,g_south,h_620,q_90,w_620/v1400795212/qfawhboadlc5dcc5u6cs.jpg',
+    'price': '999',
+    },
+    {
+    'name': 'Space cat clean combo',
+    'description': ('Space cat clean combo Space cat clean combo Space cat clean combo Space cat clean combo Space cat clean combo Space cat clean combo Space cat clean combo'),
+    'image': 'https://pguards.ru/upload/iblock/252/2523a17782d0fd5ba435d94e1465f728.jpg',
+    'price': '999',
+    },
+    {
+    'name': 'Pomelo clean combo',
+    'description': ('Pomelo clean combo Pomelo clean combo Pomelo clean combo Pomelo clean combo Pomelo clean combo Pomelo clean combo Pomelo clean combo Pomelo clean combo'),
+    'image': 'https://www.bigtv.ru/storage/goodsImages/611/611653/1000x1000-611653_1.jpg',
+    'price': '999',
+    },
+    {
+    'name': 'Pomelo space reverb effect pedal',
+    'description': ('Pomelo space reverb effect pedal Pomelo space reverb effect pedal Pomelo space reverb effect pedal Pomelo space reverb effect pedal Pomelo space reverb effect pedal'),
+    'image': 'https://dmtrpedals.ru/wp-content/uploads/2019/10/space-race-3.jpg',
+    'price': '999',
+    },
+]
--- Space Cat POMELO/app/views.py
+++ Space Cat POMELO/app/views.py
@@ -5,42 +5,65 @@ Definition of views.
 from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpRequest
+from .models import product_db
+
 
 STATIC_URL = '/static/'
 
 def home(request):
-    """Renders the home page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/index.html',
-        {
-            'title':'Home Page',
-            'year':datetime.now().year,
+    name_product = ''
+    description_product = ''
+    image_product = ''
+    price_product = ''
+    product_card = ''
+
+    for i in range(len(product_db)):
+        product_card += (f'<div class="card-horizontal-scroll">'
+                        f'<a href="/marketplace/{i}/"><img src="{product_db[i]["image"]}" class=card-image></a>'
+                        f'<h3 class="card-title"><a href="/marketplace/{i}/" class="white">{product_db[i]["name"]}</a></h3></div>')
+
+    context = {
+        'title': 'Space Cat homepade!',
+        'product_card': product_card,
         }
-    )
+    return render(request, 'app/index.html', context)
+#--------------------------------------------------------------------------------------------------------------------
 
 def marketplace(request):
     """Renders the marketplace page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-            request, 
-            'app/marketplace.html', 
-        {
-            'title':'Marketplace',
-            'year':datetime.now().year,
+    product_card = ''
 
-        })
-def product(request):
+    for i in range(len(product_db)):
+        product_card += (f'<div class="card">'
+                        f'<a href="/marketplace/{i}/"><img src="{product_db[i]["image"]}" class=card-image></a>'
+                        f'<h3 class="card-title"><a href="/marketplace/{i}/" class="white">{product_db[i]["name"]}</a></h3></div>')
+    context = {
+        'title': 'Marketplace',
+        'product_card': product_card,
+        }
+
+    assert isinstance(request, HttpRequest)
+    return render(request, 'app/marketplace.html', context)
+def product(request, pk):
     """Renders the product."""
+    name_product = product_db[pk]["name"]
+    description_product = product_db[pk]["description"]
+    image_product = product_db[pk]["image"]
+    price_product = product_db[pk]["price"]
+
+    context = {
+        'title': name_product,
+        'name_product': name_product,
+        'description_product': description_product,
+        'image_product': image_product,
+        'price_product': price_product,
+        }
+
     assert isinstance(request, HttpRequest)
     return render(
         request,
         'app/product.html',
-        {
-            'title':'product',
-            'year':datetime.now().year,
-        }
+        context
     )
 
 def about(request):
